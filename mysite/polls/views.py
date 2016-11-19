1b5b9433921b11eab8d488011e07033f7790b94e
Jan Miszura <janmiszura@gmail.com>
2016-11-19 08:20:03 -0200

    tutorial 5

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,19 +1,34 @@
-from django.http import HttpResponseRedirect, HttpResponse
+from django.http import HttpResponseRedirect, HttpResponse, Http404
 from django.shortcuts import get_object_or_404, render
 from django.urls import reverse
+from django.utils import timezone
 from .models import Question, Choice
 
 def index(request):
-    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    """
+    Return the last five published questions (not including those set to be
+    published in the future).
+    """
+    latest_question_list = Question.objects.filter(
+        pub_date__lte=timezone.now()
+    ).order_by('-pub_date')[:5]
     context = {'latest_question_list': latest_question_list}
     return render(request, 'polls/index.html', context)
 
 def detail(request, question_id):
-    question = get_object_or_404(Question, pk=question_id)
+    try:
+        question = Question.objects.get(pk=question_id, pub_date__lte=timezone.now())
+    except Question.DoesNotExist:
+        raise Http404("Question does not exist")
+
     return render(request, 'polls/detail.html', {'question': question})
 
 def results(request, question_id):
-    question = get_object_or_404(Question, pk=question_id)
+    try:
+        question = Question.objects.get(pk=question_id, pub_date__lte=timezone.now())
+    except Question.DoesNotExist:
+        raise Http404("Question does not exist")
+
     return render(request, 'polls/results.html', {'question': question})
 
 def vote(request, question_id):
